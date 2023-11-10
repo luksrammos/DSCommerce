b9503204f34258c63558d8a8149ee1f784bf8517
Lucas Ramos <lucas.rsilva32@gmail.com>
2023-11-09 23:12:26 -0300

    GET product by id

---
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dscommerce.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.repositories.ProductRepositoriy;
+
+@Service
+public class ProductService {
+   
+  @Autowired
+  private ProductRepositoriy repositoriy;  
+
+  @Transactional(readOnly = true)
+  public ProductDTO findById(Long id){
+    Product product = repositoriy.findById(id).get();
+    return new ProductDTO(product);     
+  }
+
+
+
+}
